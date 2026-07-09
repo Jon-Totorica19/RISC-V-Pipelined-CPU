962d653fdff04680ad88674052463a3ef6140901
Jon Totorica <Jttoto7@gmail.com>
2026-07-09 13:24:25 -0400

    Initial commit: single-cycle RV32I baseline

---
+++ rtl/core/pc.sv
@@ -0,0 +1,15 @@
+// Program Counter: Holds the current PC value and increments on each clock edge. PC value used to address the instruction memory
+module pc (
+    input logic clk, rst,
+    input logic [31:0] next_pc,
+    output logic [31:0] pc_addr
+);
+
+    always_ff @(posedge clk) begin
+        if (rst) 
+        pc_addr <= 0;
+        else
+        pc_addr <= next_pc;
+    end
+
+endmodule
